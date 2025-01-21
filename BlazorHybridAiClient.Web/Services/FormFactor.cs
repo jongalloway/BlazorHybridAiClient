064a9b95569f77a994cd55c1e5f22f90912496c7
Jon Galloway <jongalloway@gmail.com>
2025-01-20 19:46:36 -0800

    Generic rename

---
+++ BlazorHybridAiClient.Web/Services/FormFactor.cs
@@ -0,0 +1,17 @@
+using BlazorHybridAiClient.Shared.Services;
+
+namespace BlazorHybridAiClient.Web.Services
+{
+    public class FormFactor : IFormFactor
+    {
+        public string GetFormFactor()
+        {
+            return "Web";
+        }
+
+        public string GetPlatform()
+        {
+            return Environment.OSVersion.ToString();
+        }
+    }
+}
---
+++ BlazorHybridAiClient/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorHybridAiClient
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
